84fa99771f78457cb3bd89cd61e596f1fb12cd9f
Wuming77 <2313432829@qq.com>
2021-07-24 11:59:08 +0800

    first commit

---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPerfab;
+    public GameObject powupPrefab;
+
+    public int enemyCount;
+    public int waveNumber = 1;
+
+    private float spawnRange = 9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powupPrefab, GenerateSpawnPosition(), powupPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            Instantiate(powupPrefab, GenerateSpawnPosition(), powupPrefab.transform.rotation);
+            SpawnEnemyWave(waveNumber);
+
+        }
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPerfab, GenerateSpawnPosition(), enemyPerfab.transform.rotation);
+        }
+    }
+}
